cf10d84a194582be5bf61a1996420c02db2e1f62
OSX-RSPlug-a <log-n@outlook.com>
2025-05-12 23:39:46 -0300

    Add this automation

---
+++ lowcase_to_uppercase.py
@@ -0,0 +1,15 @@
+my_string = " VARIABLE_NAME_UPPER  VARIABLE_NAME_UPPER1  VARIABLE_NAME_UPPER2 "
+
+uppercase_string = my_string.lower()
+
+print(uppercase_string)
+
+def switch_underscore_hyphen(input_string):
+    return input_string.replace('_', '#TEMP#').replace('-', '_').replace('#TEMP#', '-')
+
+# Example usage
+mixed_string = uppercase_string
+switched_string = switch_underscore_hyphen(mixed_string)
+
+print(f"Original: {mixed_string}")
+print(f"Switched: {switched_string}")

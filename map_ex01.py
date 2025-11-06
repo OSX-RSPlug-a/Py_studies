895389c2c345add811c30b676071cfdbf70d1073
OSX-RSPlug-a <Log-N@outlook.com>
2025-11-06 20:23:38 -0300

    Added map ex

---
+++ map_ex01.py
@@ -0,0 +1,24 @@
+from typing import List
+
+prices = [11.20, 25.99, 51.00, 100.10]
+
+
+def tax_apply(price: float) -> float:
+    return round(price * 1.5, 2)
+
+    
+    
+
+taxed_price = []
+for price in prices:
+    new_price = tax_apply(price)
+    taxed_price.append(new_price)
+
+print("############################# Ex without map ##################################")
+print(taxed_price)
+print("############################# !!!!!!!!!!!!!! ##################################")
+
+
+print("############################# Ex using map ##################################")
+taxed_price_mapped = list(map(tax_apply, prices))
+print(taxed_price_mapped)
